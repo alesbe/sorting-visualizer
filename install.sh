26ba9966b710f7d20c801a9cd201fe4f69102891
alesbe <aesparzabellver@gmail.com>
2022-05-14 15:45:58 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+compile() {
+	# Compile and clean
+	echo Compiling visualizer...
+
+	g++ *.cpp -o sorting-visualizer -lsfml-graphics -lsfml-window -lsfml-system
+	rm -rf *.o
+}
+
+main() {
+	# Select option
+	echo "Where do you want to install the visualizer?"
+	echo "1.- Compile and leave the executable in this folder"
+	echo "2.- Compile and move the executable into /usr/bin (requires root)"
+	echo ""
+	read -n 1 -p "Option: " option
+	echo ""
+
+	if [ "$option" = "1" ]; then
+		compile
+		echo "Done! run ./sorting-visualizer to execute"
+		exit
+
+	elif [ "$option" = "2" ]; then
+		# Check if has been runned as sudo
+		if [ "$EUID" -ne 0 ]
+			then echo "Please, run as root! (sudo ./install.sh)"
+			exit
+		fi
+
+		compile
+
+		# Move executable to /usr/bin
+		echo Moving executable into /usr/bin...
+		mv ./sorting-visualizer /usr/bin
+		echo Done! Run sorting-visualizer to open it
+		exit
+	fi
+}
+
+main
