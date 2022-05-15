67b47efb4761f41bd330ea4c45284adf77b72199
alesbe <aesparzabellver@gmail.com>
2022-05-15 12:37:03 +0200

    Improved Sortable comments

--- Sortable.cpp
+++ Sortable.cpp
@@ -1,11 +1,23 @@
 #include "Sortable.h"
 
+/**
+ * @brief Construct a new Sortable object. Sortables are the elements that comforms the main sorting array
+ * 
+ * @param width width of the sortable on screen
+ * @param height height of the sortable on screen
+ * @param value associated value to each element used to sort
+ */
 Sortable::Sortable(float width, float height, int value) {
 	this->width = width;
 	this->height = height;
 	this->value = value;
 }
 
+/**
+ * @brief Defines a shape for the sortable
+ * 
+ * @return sf::RectangleShape prepared to be directly drawed on screen
+ */
 sf::RectangleShape Sortable::shape() {
 	return sf::RectangleShape(sf::Vector2f((float)width, (float)height));
 }
