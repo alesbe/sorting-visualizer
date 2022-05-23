bc21613630f600e7c454c7c32c907d4e0ee14975
alesbe <aesparzabellver@gmail.com>
2022-05-23 18:27:02 +0200

    Fixed install.sh identations

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 main() {
 	echo "Building Makefile..."
-    cmake CMakeLists.txt
-    
-    echo ""
-    echo "Compiling project..."
-    make
+	cmake CMakeLists.txt
+	
+	echo ""
+	echo "Compiling project..."
+	make
 
-    echo ""
-    echo "Would you like to move the visualizer to /usr/bin? (Requires root)"
-    read -n 1 -p "yes / no: " option
+	echo ""
+	echo "Would you like to move the visualizer to /usr/bin? (Requires root)"
+	read -n 1 -p "yes / no: " option
 	echo ""
 	echo ""
 
 	if [ "$option" = "y" ]; then
-	    sudo mv ./sorting-visualizer /usr/bin
+		sudo mv ./sorting-visualizer /usr/bin
 	fi
 
-    echo "Done!"
+	echo "Done!"
 	exit
 }
 
