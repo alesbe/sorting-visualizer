8ce3db1f104399eff8b06da89529347951b22a69
alesbe <aesparzabellver@gmail.com>
2022-05-23 14:53:42 +0200

    Improved comments and run commands as sudo

--- install.sh
+++ install.sh
@@ -1,56 +1,40 @@
 #!/usr/bin/env bash
 compile() {
-	# determine if sfml is already installed,
-	# if its not then install it
+	# Detect is SFML libraries are installed, if not, install them
 	ldconfig -p | grep -q libsfml
 	if [ $? -ne 0 ]; then
 		install_sfml
 	fi
 
-	# Compile; Makefile is not used for now because
-	# it is not working as intended
+	# Compile visualizer
 	echo Compiling visualizer...
 	g++ ./src/*.cpp -o sorting-visualizer -lsfml-graphics -lsfml-window -lsfml-system
 }
 
 install_sfml() {
-	# detect the package manager and install sfml
-	# accordingly
+	# Detect packet manager
 
-	# we need to be root to install packages
-	if [ "$EUID" -ne 0 ]; then
-		echo "The SFML library needs to be installed to compile the program."
-		echo "This script can attempt to install SFML using the system package manager if run as root."
-		echo "Please run it again as root (sudo ./install.sh) or install SFML yourself."
-		exit
-	fi
-
-	# DETECT OS
-
-	# arch
+	# Arch
 	if command -v pacman &>/dev/null; then
-		pacman -S sfml
+		sudo pacman -S sfml
 	
-	# debian
+	# Debian
 	elif command -v apt-get &>/dev/null; then
-		apt-get install libsfml-dev
+		sudo apt-get install libsfml-dev
 
-	# fedora
+	# Fedora
 	elif command -v dnf &>/dev/null; then
-		dnf install SFML
+		sudo dnf install SFML
 
-	# void linux (not tested)
+	# Void linux (not tested)
 	elif command -v xbps &>/dev/null; then
 		# add multilib repo
-		xbps-install -Su void-repo-multilib
-
+		sudo xbps-install -Su void-repo-multilib
 		# install sfml
-		xbps-install -Su SFML-32bit
+		sudo xbps-install -Su SFML-32bit
 
-	# packet manager not found
+	# Packet manager not found
 	else
-		# package manager was not found
-		# maybe compile sfml ourselves?
 		# TODO: Implement issue #10 fix here
 		echo "Could not install SFML using the system package manager."
 		echo "Please install SFML yourself and run this script again."
@@ -62,18 +46,20 @@ main() {
 	# Select option
 	echo "Where do you want to install the visualizer?"
 	echo "1.- Compile and leave the executable in this folder"
-	echo "2.- Compile and move the executable into /usr/bin (requires root)"
+	echo "2.- Compile and move the executable into /usr/bin"
 	echo ""
 	read -n 1 -p "Option: " option
 	echo ""
 	echo ""
 
+	# Compile
 	if [ "$option" = "1" ]; then
 		compile
 
 		echo "Done!"
 		exit
 
+	# Compile and move binary to /usr/bin
 	elif [ "$option" = "2" ]; then
 		# Check if has been runned as sudo
 		if [ "$EUID" -ne 0 ]
