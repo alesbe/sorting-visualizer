8cb46f63dea71d0b8c403e9186ce14d99eae8864
alesbe <aesparzabellver@gmail.com>
2022-05-24 12:33:27 +0200

    Updated install.sh default binary location

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 main() {
+	# Build Makefile
 	echo "Building Makefile..."
 	cmake CMakeLists.txt
 	
+	# Compile using Makefile
 	echo ""
 	echo "Compiling project..."
 	make
 
+	# Select binary location
 	echo ""
 	echo "Would you like to move the visualizer to /usr/bin? (Requires root)"
-	read -n 1 -p "yes / no: " option
+	read -n 1 -p "yes (default) / no: " option
 	echo ""
 	echo ""
 
-	if [ "$option" = "y" ]; then
-		sudo mv ./sorting-visualizer /usr/bin
+	# Option 1: Leave the binary in the same folder (project root)
+	if [ "$option" = "n" ]; then
+		echo "Done! Run ./sorting-visualizer"
+		exit
 	fi
-
-	echo "Done!"
+	
+	# Option 2: Move the binary to /usr/bin
+	sudo mv ./sorting-visualizer /usr/bin
+	echo "Done! Run sorting-visualizer"
+	
 	exit
 }
 
